966dc45295ab3236ab758b85c7dd627608afb0f6
Ashwini <salunkheashwini1998@gmail.com>
2023-01-30 16:14:11 +0530

    EvenOdd

---
+++ EvenOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class EvenOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            System.Console.WriteLine("Enter the Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"Entered Number {num} is Even.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Entered Number {num} is Odd.");
+            }
+
+        }
+    }
+}
