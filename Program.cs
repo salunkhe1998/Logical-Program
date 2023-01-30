cb37a9e18114b3a4a41c9e7c858b31d2f89680f3
Ashwini <salunkheashwini1998@gmail.com>
2023-01-30 15:31:28 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LogicalProgramAll
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("******** WELCOME TO LOGICAL PROGRAMMING ");
+
+            //PrimeFactor.PrimeFactorization();
+            //Harmonic.Harmonicnumber();
+            //PrimeFactor.PrimeFactorization();
+            //Factorial.CalculateFactorial();
+            // EvenOdd.CheckEvenOdd();
+            //SwapTwoNum.TwoNumSwap();
+            //LargestNumber.FindLargestNum();
+            //VowelOrConstant.CheckVowelConstant();
+            //QuotientRemainder.CalculateQuotientAndRemainder();
+            //Fibonacci.FindFibonacci();
+            //PerfectNumber.CheckPerfectNumber();
+            //PrimeNo.CheckPrimeNumber();
+            //ReverseNumber.CheckReverseNumber();
+            //Coupon.CheckCoupon();
+            //StopWatch.StartStopwatch();
+
+
+        }
+
+    }
+}
