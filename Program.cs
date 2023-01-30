bac6a5f4127a949b705c726969bb0daade2abd0e
Ashwini <salunkheashwini1998@gmail.com>
2023-01-30 15:55:18 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LogicalProgramAll
+﻿
+
+namespace LogicalProgramAll
 {
     class Program
     {
@@ -8,21 +10,7 @@
 
             Console.WriteLine("******** WELCOME TO LOGICAL PROGRAMMING ");
 
-            //PrimeFactor.PrimeFactorization();
-            //Harmonic.Harmonicnumber();
-            //PrimeFactor.PrimeFactorization();
-            //Factorial.CalculateFactorial();
-            // EvenOdd.CheckEvenOdd();
-            //SwapTwoNum.TwoNumSwap();
-            //LargestNumber.FindLargestNum();
-            //VowelOrConstant.CheckVowelConstant();
-            //QuotientRemainder.CalculateQuotientAndRemainder();
-            //Fibonacci.FindFibonacci();
-            //PerfectNumber.CheckPerfectNumber();
-            //PrimeNo.CheckPrimeNumber();
-            //ReverseNumber.CheckReverseNumber();
-            //Coupon.CheckCoupon();
-            //StopWatch.StartStopwatch();
+            Coupon.CheckCoupon();
 
 
         }
