cfcc650a64a80458a6fb6f6239a61371f06a25de
Ashwini <salunkheashwini1998@gmail.com>
2023-02-02 15:26:41 +0530

    quetient reamainder number

---
+++ QuotientRemainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class QuotientRemainder
+    {
+        public static void CalculateQuotientAndRemainder()
+        {
+            Console.WriteLine("Enter the Dividend: ");
+            double dividend = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Diviser: ");
+            double diviser = Convert.ToDouble(Console.ReadLine());
+
+            double quotient = dividend / diviser;
+
+            double remainder = dividend % diviser;
+
+            Console.WriteLine("The Quotient is = " + quotient + " remainder = " + remainder);
+        }
+
+    }
+}
