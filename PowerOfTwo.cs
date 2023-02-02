b4f2be41e89f6ecd148f135058ad6cafab6e0b2c
Ashwini <salunkheashwini1998@gmail.com>
2023-02-02 14:48:47 +0530

    PowerOfTwo

---
+++ PowerOfTwo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class PowerofTwo
+    {
+        public static void power()
+        {
+            Console.WriteLine("Enter the number N : ");
+
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int PowerofTwo = 1;
+
+            if (0 <= N && N < 31)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    PowerofTwo = 2 * PowerofTwo;
+                }
+
+                Console.WriteLine("Power of Two is :" + PowerofTwo);
+
+            }
+            else
+            {
+                Console.WriteLine("Number is greater than 31");
+            }
+
+
+
+        }
+    }
+}
