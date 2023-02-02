788bdb16e9727b286add46ed620e9d09f8ef152f
Ashwini <salunkheashwini1998@gmail.com>
2023-02-02 11:36:27 +0530

    Largest Number Program

---
+++ LargestNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class LargestNumber
+    {
+        public static void FindLargestNum()
+        {
+            Console.WriteLine("Enter the number A : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number B : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number C : ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("A is Greater number than B & C");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("B is the Greater number than A & C");
+            }
+            else
+            {
+                Console.WriteLine("C is the Greater number than A & B");
+            }
+
+        }
+    }
+}
